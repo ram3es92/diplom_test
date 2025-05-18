e43e0dd3212cf59bc58f11674960d979d17baea7
Roman knyazev <ram3es92@mail.ru>
2025-05-18 17:41:51 +0300

    изменил вит конфиг

--- ecommerce-frontend/vite.config.ts
+++ ecommerce-frontend/vite.config.ts
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      // прокси для API-запросов к бэкенду
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
+    host: true,
+    port: 5173,
+  proxy: {
+    '/api': 'http://backend:3001'
+  }
+  }
 })
